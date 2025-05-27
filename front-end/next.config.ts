bf22f935e61c555dceef093148d1ca349713eab8
doublefire.chen <doublefire.chen@aka.cy>
2025-05-27 19:09:52 +0200

    disable Image optimization for static export

--- front-end/next.config.ts
+++ front-end/next.config.ts
@@ -1,8 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: 'export',
-  /* config options here */
-};
+  images: {
+    unoptimized: true
+  }
+}
 
-export default nextConfig;
+module.exports = nextConfig
